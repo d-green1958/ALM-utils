ee7670ae2426cc91b439ac214901ba10fff20915
dylan <dylan.green@eng.ox.ac.uk>
2024-11-13 17:57:12 +0000

    created the new python package

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="pyALMTree",
+    version="1.0",
+    description="utilities for the ALM workflow",
+    packages=find_packages(),
+    install_requires=[],  
+)
