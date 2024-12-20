e63431e24e656ab94fdbb343bfaae5715cf24c04
dylan <dylan.green@eng.ox.ac.uk>
2024-12-20 16:32:57 +0000

    added a function to read in the turbineOutput file

--- python/pyALMTree/__init__.py
+++ python/pyALMTree/__init__.py
@@ -1,2 +1,3 @@
 from . import sample
 from . import phase_average
+from . import read
---
+++ python/pyALMTree/read/turbineOutput/read_file.py
@@ -0,0 +1,13 @@
+import pandas as pd
+def read(file_path):
+    with open(file_path, 'r') as f:
+        header_line = f.readline().strip()
+        # Split the header by four spaces
+        headers = header_line.split('    ')
+        
+    # Read the rest of the file as a DataFrame
+    data = pd.read_csv(file_path, delimiter=r'\s+', skiprows=1, header=None)
+
+    # Assign the headers to the DataFrame
+    data.columns = headers
+    return data
