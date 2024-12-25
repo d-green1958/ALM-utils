af59c7c74737a941b772ff5bef60287a4d5f2c2a
dylan <dylan.green@eng.ox.ac.uk>
2024-12-25 21:56:51 +0000

    updated the plotCase script. Updated the thrust plot script in plot/turbineOutput

--- python/pyALMTree/plot/turbineOutput/__init__.py
+++ python/pyALMTree/plot/turbineOutput/__init__.py
@@ -1,3 +1,13 @@
-from .thrust import thrust, thrust_FFT, thrust_FFT_log_log
+from .thrust import thrust, thrust_FFT, thrust_FFT_log_log, thrust_details
+from .torque import torque, torque_FFT, torque_FFT_log_log, torque_details
 
-__all__ = [thrust, thrust_FFT, thrust_FFT_log_log]
+__all__ = [
+    thrust,
+    thrust_FFT,
+    thrust_FFT_log_log,
+    thrust_details,
+    torque,
+    torque_FFT,
+    torque_FFT_log_log,
+    torque_details,
+]
--- python/pyALMTree/plot/turbineOutput/thrust.py
+++ python/pyALMTree/plot/turbineOutput/thrust.py
@@ -5,7 +5,7 @@ from pyALMTree.read.turbineOutput import turbineOutput_file as read_file
 import PyhD
 
 
-def thrust(case_path, verbose=True):
+def thrust(case_path, cut_time=0, verbose=True, save_path=None):
     PyhD.matplotlib.style.apply_style()
     turbineOutput_path = os.path.join(case_path, "turbineOutput")
     turbineOutput_path = os.path.join(
@@ -20,6 +20,8 @@ def thrust(case_path, verbose=True):
         print(f"plotting thrust")
 
     df = read_file(thrust_path)
+    df = df[df["Time(s)"] >= cut_time]
+    
     figure, axs = PyhD.matplotlib.plot_helpers.landscape_fig(
         fig_name="Thrust",
         x_arrs=[df["Time(s)"]],
@@ -28,11 +30,19 @@ def thrust(case_path, verbose=True):
         y_label="Thrust [N]",
         title="Thrust Time Series",
     )
+    
+
+    if not save_path == None:
+        fig_path = os.path.join(save_path, "thrust")
+        figure.savefig(fig_path, transparent=False)
+        figure.savefig(fig_path + "_transparent", transparent=True)
+        
+    figure.tight_layout()
 
     return figure, axs
 
 
-def thrust_FFT(case_path, verbose=True):
+def thrust_FFT(case_path, cut_time=0, verbose=True, save_path=None):
     PyhD.matplotlib.style.apply_style()
     turbineOutput_path = os.path.join(case_path, "turbineOutput")
     turbineOutput_path = os.path.join(
@@ -47,9 +57,10 @@ def thrust_FFT(case_path, verbose=True):
         print(f"plotting thrust FFT")
 
     df = read_file(thrust_path)
+    df = df[df["Time(s)"] >= cut_time]
 
-    timeseries = df["thrust (N)"]
-    timevalues = df["Time(s)"]
+    timeseries = np.array(df["thrust (N)"])
+    timevalues = np.array(df["Time(s)"])
 
     n = len(timeseries)
 
@@ -59,7 +70,7 @@ def thrust_FFT(case_path, verbose=True):
     timeseries_FT = timeseries_FT[freqs >= 0]
     freqs = freqs[freqs >= 0]
 
-    timeseries_FT = 2.0 * timeseries_FT / n
+    timeseries_FT = 2.0 * np.abs(timeseries_FT) / n
 
     figure, axs = PyhD.matplotlib.plot_helpers.landscape_fig(
         fig_name="Thrust FFT",
@@ -72,10 +83,77 @@ def thrust_FFT(case_path, verbose=True):
         legend=True,
     )
 
+    figure.tight_layout()
+    
+    if not save_path == None:
+        fig_path = os.path.join(save_path, "thrust_FFT")
+        figure.savefig(fig_path, transparent=False)
+        figure.savefig(fig_path + "_transparent", transparent=True)
+        
+
+    return figure, axs
+
+def thrust_details(case_path, cut_time=0, verbose=True, save_path=None):
+    PyhD.matplotlib.style.apply_style()
+    turbineOutput_path = os.path.join(case_path, "turbineOutput")
+    turbineOutput_path = os.path.join(
+        turbineOutput_path, os.listdir(turbineOutput_path)[0]
+    )
+    thrust_path = os.path.join(turbineOutput_path, "thrust")
+
+    if not os.path.exists(thrust_path):
+        FileNotFoundError("thrust file not found")
+
+    if verbose:
+        print(f"plotting thrust details")
+
+    df = read_file(thrust_path)
+    df = df[df["Time(s)"] >= cut_time]
+
+    timeseries = np.array(df["thrust (N)"])
+    timevalues = np.array(df["Time(s)"])
+    
+    n = len(timeseries)
+
+    timeseries_FT = np.fft.fft(timeseries)
+    freqs = np.fft.fftfreq(n, d=timevalues[1] - timevalues[0])
+
+    timeseries_FT = timeseries_FT[freqs >= 0]
+    freqs = freqs[freqs >= 0]
+
+    timeseries_FT = 2.0 * np.abs(timeseries_FT) / n
+
+    # figure, axs = plt.subplots()  # Adjust the size as needed
+    figure = plt.figure(num="Thrust Details")
+    axs = figure.gca()
+    axs.axis('off')  
+    
+    column_labels = ["Parameter", "Value"]
+    table_data = [
+        ["Mean Value", f"{np.mean(df["thrust (N)"]):.5g}"],
+        ["Non-Zero Peak Location", f"{(freqs[freqs > 0])[np.argmax(timeseries_FT[freqs > 0])]:.5g}"],
+        ["Non-Zero Peak Value", f"{np.real((timeseries_FT[freqs > 0])[np.argmax(timeseries_FT[freqs > 0])]):.5g}"],
+        ["Non-Zero Peak-to-Peak Value", f"{2.0*np.real((timeseries_FT[freqs > 0])[np.argmax(timeseries_FT[freqs > 0])]):.5g}"]
+    ]
+
+    table = axs.table(
+        cellText=table_data,  # Data for the cells
+        colLabels=column_labels,  # Column headers
+        loc='center',  # Center the table
+        cellLoc='center'  # Center text within cells
+    )
+
+    figure.tight_layout()
+    
+    if not save_path == None:
+        fig_path = os.path.join(save_path, "thrust_details")
+        figure.savefig(fig_path, transparent=False)
+        figure.savefig(fig_path + "_transparent", transparent=True)
+        
     return figure, axs
 
 
-def thrust_FFT_log_log(case_path, verbose=True):
+def thrust_FFT_log_log(case_path, cut_time=0, verbose=True, save_path=None):
     PyhD.matplotlib.style.apply_style()
     turbineOutput_path = os.path.join(case_path, "turbineOutput")
     turbineOutput_path = os.path.join(
@@ -90,9 +168,10 @@ def thrust_FFT_log_log(case_path, verbose=True):
         print(f"plotting thrust FFT log-log")
 
     df = read_file(thrust_path)
+    df = df[df["Time(s)"] >= cut_time]
 
-    timeseries = df["thrust (N)"]
-    timevalues = df["Time(s)"]
+    timeseries = np.array(df["thrust (N)"])
+    timevalues = np.array(df["Time(s)"])
 
     n = len(timeseries)
 
@@ -102,7 +181,7 @@ def thrust_FFT_log_log(case_path, verbose=True):
     timeseries_FT = timeseries_FT[freqs > 0]
     freqs = freqs[freqs > 0]
 
-    timeseries_FT = 2.0 * timeseries_FT / n
+    timeseries_FT = 2.0 * np.abs(timeseries_FT) / n
 
     figure, axs = PyhD.matplotlib.plot_helpers.landscape_fig(
         fig_name="Thrust FFT log-log",
@@ -117,5 +196,12 @@ def thrust_FFT_log_log(case_path, verbose=True):
 
     axs.set_xscale("log")
     axs.set_yscale("log")
+    
+    figure.tight_layout()
+    
+    if not save_path == None:
+        fig_path = os.path.join(save_path, "thrust_FFT_log_log")
+        figure.savefig(fig_path, transparent=False)
+        figure.savefig(fig_path + "_transparent", transparent=True)
 
     return figure, axs
